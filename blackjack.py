4021ab14beaa93cde62059386139265a485c537e
CannibalPoop <108429054+CannibalPoop@users.noreply.github.com>
2022-07-19 17:58:11 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -10,16 +10,16 @@ class Deck:
     def __init__(self):
         self.list_of_cards = ["Hearts", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], ["Diamonds", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], ["Spades", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"], ["Clubs", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]    
 
-    def draw_card_closed(self):
+    def draw_card_closed(self, hand_or_table: list):
         c = Card()
         for i in range(4):
             if self.list_of_cards[i][0] == c.suit:
                 if c.value in self.list_of_cards[i]:
-                    #print(c.suit, c.value)
                     self.list_of_cards[i][self.list_of_cards[i].index(c.value)] = 0
+                    hand_or_table.append(c.value)
                     break
                 else:
-                    self.draw_card_closed()            
+                    self.draw_card_closed(hand_or_table)            
         return(self)
 
     def draw_card_open(self, hand_or_table: list):
@@ -27,43 +27,59 @@ class Deck:
         for i in range(4):
             if self.list_of_cards[i][0] == c.suit:
                 if c.value in self.list_of_cards[i]:
-                    hand_or_table.append(c.value + ' of ' + c.suit)
+                    hand_or_table.append(c.value)
                     self.list_of_cards[i][self.list_of_cards[i].index(c.value)] = 0
                     break
                 else:
                     self.draw_card_open(hand_or_table)            
         return(self)
 
-def give_cards(self, hand: list):
-    print("Ваши карты")
+def sum_of_cards(hand_or_dealer: list):
+    value = 0
+    if "A" in hand_or_dealer:
+        hand_or_dealer.remove("A")
+        hand_or_dealer.append("A")
+    for i in range(len(hand_or_dealer)):
+        if hand_or_dealer[i] in ["2", "3", "4", "5", "6", "7", "8", "9", "10"]:
+            value += int(hand_or_dealer[i])
+        elif hand_or_dealer[i] == "A":
+            if value > 10:
+                value += 1
+            else:
+                value += 11
+        else:
+            value += 10
+    return value
+
+
+def give_cards(self, hand: list, dealer: list):
     [self.draw_card_open(hand) for i in range(2)]
+    print("Ваши карты: ", ', '.join(hand))
+    self.draw_card_open(dealer)
+    print("Карты дилера: ", ''.join(dealer))
+    self.draw_card_closed(dealer)
+
+def hit(self, hand):
+    self.draw_card_open(hand)
     print(', '.join(hand))
 
-def flop(self, table: list):
-    print("Flop: ")
-    self.draw_card_closed()
-    [self.draw_card_open(table) for i in range(3)]
-    print(', '.join(table))        
-
-def turn(self, table: list):
-    print("Turn:")
-    self.draw_card_closed()
-    self.draw_card_open(table)
-    print(' '.join(table)) 
-
-def river(self, table: list):
-    print("River:")
-    self.draw_card_closed()
-    self.draw_card_open(table)
-    print(', '.join(table)) 
-
+money = 1000
 d = Deck()
-h1, h2,  f, t, r = [], [], [], [], []
-counter = 0
-give_cards(d, h1)
-give_cards(d, h2)
-flop(d, f)
-turn(d, t)
-river(d, r)
-table = f + t + r
-print(table)
+h, dealer = [], []
+bet = 100
+    #money, int(input("Добро пожаловать в казино 'Гнездо'. На вашем балансе 1000 яиц. Делайте ваши ставки! ")))
+give_cards(d, h, dealer)
+answer = input("Ваши действия?")
+while answer == "h":
+    hit(d, h)
+    answer = input("Ваши действия?")
+while sum_of_cards(dealer) < 16:
+    hit(d, dealer)
+print("Ваш счет: ", sum_of_cards(h), '\n', "Счет дилера: ", sum_of_cards(dealer))
+if (sum_of_cards(h) < 22 and sum_of_cards(h) > sum_of_cards(dealer)) or (sum_of_cards(h) < 22 and sum_of_cards(dealer) > 21):
+    print("Вы победили!")
+    money += bet
+else:
+    print("Удачи в следующий раз!")
+    money -= bet
+print("Ваш баланс: ", money)
